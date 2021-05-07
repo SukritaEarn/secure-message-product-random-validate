6d53859c1ac12d2936767e2277c2aa6172144d34
SukritaEarn <sukrita.ki@ku.th>
2021-05-07 19:27:46 +0700

    separate DTO and DAO

--- src/main/java/ku/message/model/Message.java
+++ src/main/java/ku/message/model/Message.java
@@ -2,9 +2,8 @@ package ku.message.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.Instant;
 import java.util.UUID;
 
 @Data
@@ -15,7 +14,7 @@ public class Message {
     @Id
     @GeneratedValue
     private UUID id;
-
     private String user;
     private String text;
+    private Instant createdAt;
 }
